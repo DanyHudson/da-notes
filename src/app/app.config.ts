85d216d48ebe937dd8cc4f87f9bbd4be47526867
DanyHudson <mail.d.hudson@gmail.com>
2025-10-16 23:01:51 +0200

    fixed issue with obsolete properties

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,16 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "da-notes-68be2", appId: "1:471750333561:web:e7828fc2b047a0ef06b0a8", storageBucket: "da-notes-68be2.firebasestorage.app", apiKey: "AIzaSyDiROWrNbSntp5_PmWgMUeOtqZ5txUkQjs", authDomain: "da-notes-68be2.firebaseapp.com", messagingSenderId: "471750333561", projectNumber: "471750333561", version: "2" })), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "da-notes-68be2", appId: "1:471750333561:web:680ce226d397425806b0a8", storageBucket: "da-notes-68be2.firebasestorage.app", apiKey: "AIzaSyDiROWrNbSntp5_PmWgMUeOtqZ5txUkQjs", authDomain: "da-notes-68be2.firebaseapp.com", messagingSenderId: "471750333561", projectNumber: "471750333561", version: "2" })), provideFirestore(() => getFirestore())]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "da-notes-68be2",
+      appId: "1:471750333561:web:e7828fc2b047a0ef06b0a8",
+      storageBucket: "da-notes-68be2.firebasestorage.app",
+      apiKey: "AIzaSyDiROWrNbSntp5_PmWgMUeOtqZ5txUkQjs",
+      authDomain: "da-notes-68be2.firebaseapp.com",
+      messagingSenderId: "471750333561"
+    })),
+    provideFirestore(() => getFirestore())
+  ]
 };
