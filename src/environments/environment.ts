f9594487e72eca31477e02df1fe9d6f661acc242
DanyHudson <mail.d.hudson@gmail.com>
2025-10-16 15:18:25 +0200

    added enviroment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDiROWrNbSntp5_PmWgMUeOtqZ5txUkQjs",
+    authDomain: "da-notes-68be2.firebaseapp.com",
+    projectId: "da-notes-68be2",
+    storageBucket: "da-notes-68be2.firebasestorage.app",
+    messagingSenderId: "471750333561",
+    appId: "1:471750333561:web:680ce226d397425806b0a8"
+  }
+};
