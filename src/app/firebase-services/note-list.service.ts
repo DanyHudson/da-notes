d054c1f5abcdb522954a853f0d78a1e0f1c0cd86
DanyHudson <mail.d.hudson@gmail.com>
2025-10-16 15:52:57 +0200

    added reference to firebase elements trash and notes

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  firestore = inject(Firestore);
+
   constructor() { }
+
+
+  //  itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+   // return doc(this.firestore, `${colId}/${docId}`);
+   return doc(collection(this.firestore, colId), docId);
+  }
+
+
 }
